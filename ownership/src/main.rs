df1d352f50f1e31da37c111364284935084d1da5
mulveys7619 <mulveyseanm@gmail.com>
2025-03-24 13:25:14 -0400

    Ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -40,6 +40,50 @@ fn main() {
 
     println!("{a}, world!");
 
+    let s = String::from("My String.");
+
+    let b = "My &str.";
+
+
+    // Pass s.clone() to keep s in scope
+    takes_ownership(s.clone());
+
+    let s = take_and_give_ownership(s);
+
+    println!("{s}");
+
+    str_test(b);
+
+    println!("{b}");
+
+    let c = String::from("test");
+
+    let (d, e) = calculate_len(c);
+
+    println!("The size of {d} is {e}");
+
+
+
     
 
+}
+
+fn takes_ownership(s: String){
+    println!("{s}");
+
+
+}
+
+fn str_test(s: &str){
+    println!("{s}");
+}
+
+fn take_and_give_ownership(s: String) -> String {
+    println!("{s}");
+    s
+}
+
+fn calculate_len(s: String) -> (String, usize){
+    let length = s.len();
+    (s, length)
 }
